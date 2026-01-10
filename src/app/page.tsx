00313ec280e79a20d93d67173173d6fa752a4a7c
Gideon Jackbara <gideonjackbara@gmail.com>
2026-01-10 22:13:14 +0000

    pls i want the welcome page to be exactly what is in the image attachmen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ export default function SplashPage() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.replace('/home');
-    }, 5000);
+    }, 3000); // Reduced time to 3 seconds
 
     return () => clearTimeout(timer);
   }, [router]);
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-background">
-      <div className="animation-fade-in animate-pulse">
+    <div className="flex h-screen w-full items-center justify-center bg-white">
+      <div className="animation-fade-in">
         <Logo />
       </div>
     </div>
