8f9f0e75239ec3835329a6c265ed280535a58c78
Gideon Jackbara <gideonjackbara@gmail.com>
2026-01-14 11:31:52 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,8 @@
-
-    
+export const firebaseConfig = {
+  "projectId": "studio-5677968342-6fe1f",
+  "appId": "1:814748174954:web:cb903e2d11feb5a1771813",
+  "apiKey": "AIzaSyAcHPArZWbZCzgb6ElTStvEPJn9FevhmSY",
+  "authDomain": "studio-5677968342-6fe1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "814748174954"
+};
